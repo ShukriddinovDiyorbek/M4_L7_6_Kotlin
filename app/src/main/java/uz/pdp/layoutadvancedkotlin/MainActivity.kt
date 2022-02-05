2b0f23460193ba65a4ec7006cf0301470a594018
avtech <shdiyor041@gmail.com>
2022-02-05 12:39:11 +0500

    ok

--- app/src/main/java/uz/pdp/layoutadvancedkotlin/MainActivity.kt
+++ app/src/main/java/uz/pdp/layoutadvancedkotlin/MainActivity.kt
@@ -1,11 +1,34 @@
 package uz.pdp.layoutadvancedkotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        val textView = findViewById<TextView>(R.id.textView)
+        val text =
+            "I know just how to #whister, And I know how to cry, I know just where to find the answer"
+
+        val spannable: Spannable = SpannableString(text)
+        spannable.setSpan(
+            ForegroundColorSpan(Color.BLUE),
+            18,
+            27,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        textView.text = spannable
     }
 }
